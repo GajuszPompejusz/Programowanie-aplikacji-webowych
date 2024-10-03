066c20d2f4c074b9333b9482352de1a78ad1b9cd
Jakub Wszołek <93009556+GajuszPompejusz@users.noreply.github.com>
2024-10-03 07:26:47 +0000

    1_1.1.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 (function () {
   //TODO
+  const ex1_button = document.getElementById("ex1_button");
+  const ex1_content = document.getElementById("ex1_content");
+
+  ex1_button.onclick = function () {
+    ex1_content.innerHTML = [0,1,2,3,4,5,6,7,8,9];
+  }
+  
 })();
