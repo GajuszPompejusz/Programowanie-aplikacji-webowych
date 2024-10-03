d5693c843a807ff2aa206160045c88693ea5ec99
Jakub Wszołek <93009556+GajuszPompejusz@users.noreply.github.com>
2024-10-03 07:38:54 +0000

    1_1.2.

--- script.js
+++ script.js
@@ -4,7 +4,29 @@
   const ex1_content = document.getElementById("ex1_content");
 
   ex1_button.onclick = function () {
-    ex1_content.innerHTML = [0,1,2,3,4,5,6,7,8,9];
-  }
-  
+    ex1_content.innerHTML = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  };
+
+  const ex2_text = document.getElementById("ex2_text");
+  const ex2_content = document.getElementById("ex2_content");
+
+  ex2_text.oninput = function () {
+    const text = ex2_text.value;
+    ex2_content.innerText = "";
+    if (text.length != 9) {
+      ex2_content.innerText += "Długość musi być równa 9\n";
+    }
+
+    if (text.match(new RegExp("[a-zA-Z]"))) {
+      ex2_content.innerText += "Numer nie może zawierać liter\n";
+    }
+
+    if (text.match(new RegExp("\\W"))) {
+      ex2_content.innerText += "Numer nie może zawierać znaków specjalnych\n";
+    }
+
+    if (ex2_content.innerText == "") {
+      ex2_content.innerText += "Numer jest poprawny";
+    }
+  };
 })();
